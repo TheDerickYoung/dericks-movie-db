b6830c87c72d79b393192fd56cb067b1aec609fe
Derick Young <119906716+TheDerickYoung@users.noreply.github.com>
2023-03-19 03:47:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+document.querySelector('#search-button').addEventListener('click', getSearchFetch);
+
+function getSearchFetch() {
+  const searchBar = document.querySelector('#search-bar');
+  const searchValue = searchBar.value;
+  console.log(searchValue);
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=8e6fd968cc27fdf9ffc1350938f6c87c&language=en-US&query=${searchValue}&page=1&include_adult=false`;
+
+  // Clear the input field
+  searchBar.value = '';
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.results);
+      
+      // Clear the DOM
+      clearDOM();
+      
+      data.results.forEach(element => {
+        const newDiv = document.createElement('div');
+        newDiv.setAttribute('class', 'card');
+
+        const newImg = document.createElement('img');
+        newImg.src = 'https://image.tmdb.org/t/p/w500' + element.poster_path;
+
+        const newTitle = document.createElement('h2');
+        newTitle.innerText = element.original_title;
+
+        newDiv.appendChild(newImg);
+        newDiv.appendChild(newTitle);
+
+        const parentElement = document.querySelector('.row');
+        parentElement.appendChild(newDiv);
+      });
+    });
+}
+
+// Function to clear the DOM
+function clearDOM() {
+  const parentElement = document.querySelector('.row');
+  
+  // Remove all child elements
+  while (parentElement.firstChild) {
+    parentElement.removeChild(parentElement.firstChild);
+  }
+}
