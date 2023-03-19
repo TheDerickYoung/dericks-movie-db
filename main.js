98aab48d19212e65d461a02a19979000ec5a100c
Derick Young <119906716+TheDerickYoung@users.noreply.github.com>
2023-03-19 05:13:41 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+addEventListener("load", (event) => {
+  const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=8e6fd968cc27fdf9ffc1350938f6c87c&language=en-US&page=1`
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data.results)
+
+      data.results.forEach(element => {
+        const newDiv = document.createElement('div');
+        newDiv.setAttribute('class', 'card');
+
+        const newImg = document.createElement('img');
+        newImg.src = 'https://image.tmdb.org/t/p/w500' + element.poster_path;
+
+        const newTitle = document.createElement('h2');
+        newTitle.innerText = element.original_title;
+
+        newDiv.appendChild(newImg);
+        newDiv.appendChild(newTitle);
+
+        const parentElement = document.querySelector('.row');
+        parentElement.appendChild(newDiv);
+      });
+    })
+});
+
 document.querySelector('#search-button').addEventListener('click', getSearchFetch);
 
 function getSearchFetch() {
